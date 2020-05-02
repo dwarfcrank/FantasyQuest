83f83b111fe1345bd7f5ad5786ae0c843cfdc2b3
Matias Juntunen <matias.juntunen@gmail.com>
2020-05-02 16:10:33 +0300

    make lights work properly

--- Game/PixelShader.hlsl
+++ Game/PixelShader.hlsl
@@ -10,14 +10,11 @@ struct PointLight
 {
     float4 Position; // .w = linear attenuation
     float4 Color; // .w = quadratic attenuation
-    //float Radius;
 };
-
-//static const float3 g_LightDir = float3(0.5773502691896258, 0.5773502691896258, -0.5773502691896258);
 
 StructuredBuffer<PointLight> PointLights : register(t0);
 
-float3 ComputeLight(PointLight light, float3 position, float3 normal)
+float3 ComputePointLight(PointLight light, float3 position, float3 normal)
 {
     float3 l = light.Position.xyz - position;
     float distance = length(l);
@@ -25,23 +22,25 @@ float3 ComputeLight(PointLight light, float3 position, float3 normal)
     l /= distance;
 
     float attenuation = 1.0f / (light.Position.w * distance + light.Color.w * distance * distance);
-
 	float ndotl = max(0.0f, dot(normal, l));
 
     return light.Color.rgb * ndotl * attenuation;
 }
 
+float3 ComputeDirectionalLight(float3 light, float3 normal)
+{
+    float ndotl = max(0.0f, dot(normal, light.xyz));
+    // TODO: implement color for directional light
+	return ndotl * float3(1.0f, 1.0f, 1.0f);
+}
+
 float4 main(VS_Output v) : SV_TARGET
 {
-    //float ndotl = max(0.0f, dot(v.Normal, g_LightDir));
-    float4 color = v.Color;
-
-	float ndotl = max(0.0f, dot(v.Normal, LightDir.xyz));
-    color.rgb = v.Color.rgb * ndotl;
+    float3 total = ComputeDirectionalLight(LightDir, v.Normal);
 
     for (uint i = 0; i < NumPointLights; i++) {
-        color.rgb += ComputeLight(PointLights[i], v.PositionWS.xyz, v.Normal);
+        total += ComputePointLight(PointLights[i], v.PositionWS.xyz, v.Normal);
     }
 
-    return color;
+    return v.Color * float4(total, 1.0f);
 }
